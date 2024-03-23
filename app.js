a2f2f6c7bbd0e2a961027f1bd84076335507c931
Bilal Ahmed <ba3801534@gmail.com>
2024-03-23 23:21:38 +0500

    update file

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 //  Chapter 21-25
-//Q.1
-var firstName = prompt("Enter your first Name");
-var lastName = prompt("Enter your Last Name");
+// //Q.1
+// var firstName = prompt("Enter your first Name");
+// var lastName = prompt("Enter your Last Name");
 
-alert("Asalam u alikum"+"\n"+ firstName + lastName);
+// alert("Asalam u alikum"+"\n"+ firstName + lastName);
 
-//Q.2
-var userInput = prompt("Your favorite Mobile phone")
+// //Q.2
+// var userInput = prompt("Your favorite Mobile phone")
 
-alert("My favorite phone is: " +userInput + "<br>" + "lenght of string: " + userInput.length )
+// alert("My favorite phone is: " +userInput + "<br>" + "lenght of string: " + userInput.length )
 
+//Q.3
+var word = "Pakistani"
+document.write("string:" + word)
+var word_1 = word.indexOf("n")
+
+document.write("Index Of "+ word_1);
