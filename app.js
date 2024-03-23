20fed2b0a0dca8fcdfda7c474b7546c7d8341eba
Bilal Ahmed <ba3801534@gmail.com>
2024-03-23 06:10:22 +0500

    String Method

---
+++ app.js
@@ -0,0 +1,12 @@
+//  Chapter 21-25
+//Q.1
+var firstName = prompt("Enter your first Name");
+var lastName = prompt("Enter your Last Name");
+
+alert("Asalam u alikum"+"\n"+ firstName + lastName);
+
+//Q.2
+var userInput = prompt("Your favorite Mobile phone")
+
+document.write("My favorite phone is: " +userInput + "<br>" + "lenght of string: " + userInput.length )
+
