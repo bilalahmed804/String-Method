f7d555f073159c0feedb435fe622d8539bd2ed94
Bilal Ahmed <ba3801534@gmail.com>
2024-03-27 23:26:25 +0500

    string method

--- app.js
+++ app.js
@@ -1,83 +1,83 @@
-// //  Chapter 21-25
-// //Q.1
-// var firstName = prompt("Enter your first Name");
-// var lastName = prompt("Enter your Last Name");
+//  Chapter 21-25
+//Q.1
+var firstName = prompt("Enter your first Name");
+var lastName = prompt("Enter your Last Name");
 
-// alert("Asalam u alikum"+"\n"+ firstName + lastName);
+alert("Asalam u alikum"+"\n"+ firstName + lastName);
 
-// //Q.2
-// var userInput = prompt("Your favorite Mobile phone")
+//Q.2
+var userInput = prompt("Your favorite Mobile phone")
 
-// alert("My favorite phone is: " +userInput + "<br>" + "lenght of string: " + userInput.length )
+alert("My favorite phone is: " +userInput + "<br>" + "lenght of string: " + userInput.length )
 
-// //Q.3
-// var word = "Pakistani"
-// document.write("string:" + word +"<br>")
-// var word_1 = word.indexOf("n")
+//Q.3
+var word = "Pakistani"
+document.write("string:" + word +"<br>")
+var word_1 = word.indexOf("n")
 
-// document.write("Index Of 'n' "+ word_1 +"<br>");
+document.write("Index Of 'n' "+ word_1 +"<br>");
 
-// //Q.4
-// var pharse_word = "Hello wOrld"
-// document.write("string:" + pharse_word +"<br>")
-// var word_$ = pharse_word.lastIndexOf("l")
+//Q.4
+var pharse_word = "Hello wOrld"
+document.write("string:" + pharse_word +"<br>")
+var word_$ = pharse_word.lastIndexOf("l")
 
-// document.write("Index Of 'l' "+ word_$);
-// //Q5
-// var three_word = "Pakistani"
-// document.write("<h3 style='color:blue; display:inline;'>String :</h3> " + three_word +"<br>")
-// var word_$ = three_word.indexOf("i")
+document.write("Index Of 'l' "+ word_$);
+//Q5
+var three_word = "Pakistani"
+document.write("<h3 style='color:blue; display:inline;'>String :</h3> " + three_word +"<br>")
+var word_$ = three_word.indexOf("i")
 
-// document.write("<h3 style='color:blue; display:inline;'>Character at index 3 : </h3>"+" i");
+document.write("<h3 style='color:blue; display:inline;'>Character at index 3 : </h3>"+" i");
 
-// //Q6
-// var firstName = prompt("Enter your first Name");
-// var lastName = prompt("Enter your Last Name");
+//Q6
+var firstName = prompt("Enter your first Name");
+var lastName = prompt("Enter your Last Name");
 
-// var fullName = firstName.concat(" ", lastName)
-// alert("Asalam u alikum"+"\n"+ fullName);
+var fullName = firstName.concat(" ", lastName)
+alert("Asalam u alikum"+"\n"+ fullName);
 
-// //Q7
-// var text = "Hyderabad"
-// var indexNumber = text.indexOf("Hyderabad");
+//Q7
+var text = "Hyderabad"
+var indexNumber = text.indexOf("Hyderabad");
 
-// var firstText = text.slice(0,indexNumber)
-// var replace = "Islamabad"
+var firstText = text.slice(0,indexNumber)
+var replace = "Islamabad"
 
-// document.write(firstText + replace)
+document.write(firstText + replace)
 
-// //Q8
+//Q8
 
-// var message = "Ali and Sami are best firends. The play circket and football together ";
+var message = "Ali and Sami are best firends. The play circket and football together ";
 
-// var indexNumber = message.replaceAll("and", "&")
-// console.log(indexNumber);
-
-
-// //Q.9
-
-// var strNumber = "472";
-// document.write("Value : "+strNumber + "<br>")
-// document.write("type : "+typeof strNumber +"<br>"+"<br>")
-// var number = parseInt(strNumber);
-// document.write("Value : " + number + "<br>");
-// document.write("Type : " + typeof(number));
-// document.write("<br>"+"<br>")
+var indexNumber = message.replaceAll("and", "&")
+console.log(indexNumber);
 
 
-// //Q10
-// var userInput = "javascript"
+//Q.9
 
-// console.log(userInput.toUpperCase());
+var strNumber = "472";
+document.write("Value : "+strNumber + "<br>")
+document.write("type : "+typeof strNumber +"<br>"+"<br>")
+var number = parseInt(strNumber);
+document.write("Value : " + number + "<br>");
+document.write("Type : " + typeof(number));
+document.write("<br>"+"<br>")
 
-// var userInput = prompt("Enter first name:");
-// document.write("Capitalized input: " + userInput.toUpperCase(),"<br>","<br>");
 
-// var userInput = prompt("Enter second name:");
-// var titleCase = userInput.toLowerCase().replace(/\b\w/g, function (char) {
-//     return char.toUpperCase();
-// });
-// document.write("Title case input: " + titleCase);
+//Q10
+var userInput = "javascript"
+
+console.log(userInput.toUpperCase());
+
+var userInput = prompt("Enter first name:");
+document.write("Capitalized input: " + userInput.toUpperCase(),"<br>","<br>");
+
+var userInput = prompt("Enter second name:");
+var titleCase = userInput.toLowerCase().replace(/\b\w/g, function (char) {
+    return char.toUpperCase();
+});
+document.write("Title case input: " + titleCase);
 
 // Q12
 var num3 = 35.36
